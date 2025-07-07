4a659f33f1e9bf4601ae77f1e6107a2a0523ebeb
muskan588p <musk2806@gmail.com>
2025-07-07 09:46:30 +0530

    map in js

---
+++ map.js
@@ -0,0 +1,26 @@
+// console.log("start script");
+
+// let arr=[1,2,3,4,5];
+
+// // const newarr=arr.map((x) => {x*2});   //return undefined values due to {}
+// const newarr=arr.map((x) => x*2);
+
+// console.log(newarr);
+
+let arr=[1,2,3,4,5];
+
+function doubleData(x) {
+    return x * x;
+}
+
+// Array.prototype.mapReplica=(logic)=>{
+Array.prototype.mapReplica= function(logic){
+    let outputarr=[];
+    for(let i=0; i<this.length; i++){
+        outputarr.push(logic(this[i]*2));
+    }
+    return outputarr;
+}
+
+const myprotodata=arr.mapReplica(doubleData);
+console.log(myprotodata);
