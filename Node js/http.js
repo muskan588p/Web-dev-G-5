af116aa5ebc46089c1795c771453d44279c542dd
muskan588p <musk2806@gmail.com>
2025-07-22 10:50:46 +0530

    http module

--- Node js/http.js
+++ Node js/http.js
@@ -1,10 +1,32 @@
-const http=require("http");
+// HTTP SERVER
+const http = require("node:http");
 
-const server=http.createServer((req, res)=>{
-    res.writeHead(200,{"Content-type":"text/plain"});         //text/html    //not plain/text
-    res.end("data fetched response send");
+const blogPost = [
+    {
+        "blogTitle" : "THIS IS TILE OF BLOG",
+        "blogDescription":"THIS IS DESCRIPTION OF BLOG",
+        "authorBlog": "Pransh",
+        "date":"22 July"
+
+    }
+
+]
+
+const server = http.createServer((req,res)=>{
+    if (req.method == "GET" && req.url == '/'){
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.end("DATA FETCHED RESPONSE SEND")
+    }
+    else if (req.method == "GET" && req.url == '/blog'){
+        res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify(blogPost))
+    }
+    else{
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.end("NOT FOUND")
+    }
 })
 
-server.listen(8000, ()=>{
-    console.log("server connected");
+server.listen(8000,()=>{
+    console.log("YOUR SERVER RUNNING ON PORT 8000")
 })
